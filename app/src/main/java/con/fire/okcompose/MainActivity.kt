ee056608c8653cc28e1210cebb77fc992b08f898
harris1000 <huangjinping1000@163.com>
2023-08-09 14:20:35 +0800

    ci2
    ci2

---
+++ app/src/main/java/con/fire/okcompose/MainActivity.kt
@@ -0,0 +1,42 @@
+package con.fire.okcompose
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import con.fire.okcompose.ui.theme.OkComposeTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            OkComposeTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(
+                    modifier = Modifier.fillMaxSize(), color = MaterialTheme.colorScheme.background
+                ) {
+                    Greeting("打开照相机")
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String) {
+    Text(text = "$name!")
+}
+
+@Preview(showBackground = true)
+@Composable
+fun DefaultPreview() {
+    OkComposeTheme {
+        Greeting("Android")
+    }
+}
